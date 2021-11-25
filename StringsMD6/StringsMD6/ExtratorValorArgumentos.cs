2d5a8d01d46a36b6746fb75103d08887c3e90ec0
Derick <Derick@WORKSTATION07>
2021-11-25 10:46:36 -0300

    Modulo 6, manipulação de string c#

---
+++ StringsMD6/StringsMD6/ExtratorValorArgumentos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsMD6 {
+    public class ExtratorValorArgumentos {
+        public string URL { get; }
+        public ExtratorValorArgumentos(string url) {
+
+            string.IsNullOrEmpty(url);
+
+            if (url == null) {
+                throw new ArgumentNullException(nameof(url));
+            }
+            if (url == "") {
+                throw new ArgumentException("O argumento não pode ser uma string vazia: ",nameof(url));
+            }
+            URL = url;
+        }
+
+    }
+}
---
+++ StringsMD6/StringsMD6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsMD6 {
+    internal class Program {
+        static void Main() {
+            ExtratorValorArgumentos url = new ExtratorValorArgumentos(null);
+            string urll = "pagina?argumentos";
+
+           string arg = urll.Substring(6);
+            int indice = urll.IndexOf('?');
+           Console.WriteLine(indice);
+        }
+    }
+}
