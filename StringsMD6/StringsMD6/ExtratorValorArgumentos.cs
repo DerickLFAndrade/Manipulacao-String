b28338702db307a50c664eeef2203481497b7bd8
Derick <Derick@WORKSTATION07>
2021-11-25 12:31:20 -0300

    manipulando strings

--- StringsMD6/StringsMD6/ExtratorValorArgumentos.cs
+++ StringsMD6/StringsMD6/ExtratorValorArgumentos.cs
@@ -2,22 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace StringsMD6 {
     public class ExtratorValorArgumentos {
+
+        private readonly string _argumentos;
         public string URL { get; }
         public ExtratorValorArgumentos(string url) {
 
-            string.IsNullOrEmpty(url);
+            if(string.IsNullOrEmpty(url)) {
+                throw new ArgumentException("O argumento url não pode ser nulo ou vazio", nameof(url));
+            }
 
-            if (url == null) {
-                throw new ArgumentNullException(nameof(url));
-            }
-            if (url == "") {
-                throw new ArgumentException("O argumento não pode ser uma string vazia: ",nameof(url));
-            }
+            int indiceInterrogacao = url.IndexOf('?');
+            _argumentos = URL.Substring(indiceInterrogacao);
+
             URL = url;
+        }
+        public string GetValor(string nomeParametro) {
+
+            string termo = nomeParametro + "=";
+            int indiceTermo = _argumentos.IndexOf(termo);
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceEComercial = resultado.IndexOf('&');
+
+            if (indiceEComercial == -1) {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial);
+
+        }
+
+        public Match GetNumero (string numero) {
+            Match verificar = Regex.Match(numero, "[0-9] [0-9] [0-0] [-] [0-9] [0-9] [0-0]");
+            return verificar;
         }
 
     }
--- StringsMD6/StringsMD6/Program.cs
+++ StringsMD6/StringsMD6/Program.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace StringsMD6 {
     internal class Program {
         static void Main() {
-            ExtratorValorArgumentos url = new ExtratorValorArgumentos(null);
-            string urll = "pagina?argumentos";
-
-           string arg = urll.Substring(6);
-            int indice = urll.IndexOf('?');
-           Console.WriteLine(indice);
+          //  ExtratorValorArgumentos url = new ExtratorValorArgumentos(null);
+          //  string urll = "pagina?argumentos";
+            string numero = "5454-124";
+            //ExtratorValorArgumentos ext = new ExtratorValorArgumentos("aaaa");
+            //string padrao = "[0-9][0-9][0-9][-][0-9][0-9][0-9]";
+            string padrao2 = "[0-9]{3,4}[-]{0,1}[0-9]{3}";
+            Match resultado = Regex.Match(numero, padrao2);
+            Console.WriteLine(resultado.Value);
+           // Console.WriteLine(urll.StartsWith("pagina?argumentos"));
+           //string arg = urll.Substring(6);
+           // int indice = urll.IndexOf('?');
+           //Console.WriteLine(indice);
         }
     }
 }
